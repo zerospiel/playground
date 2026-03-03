fe0c7fc7d8752060af9046c22f0c8bb87b0d7e08
Michael Morgen <mmorgen@posteo.com>
2026-03-03 16:35:23 +0100

    chore: rearrange funcs/vers

--- main.go
+++ main.go
@@ -26,121 +26,10 @@ import (
 	"weak"
 )
 
-// type foo struct {
-// 	_ structs.HostLayout
-// }
-
-type Tree[K cmp.Ordered, V any] struct {
-	left, right *Tree[K, V]
-	key         K
-	value       V
-}
-
-type TreeInt = Tree[int, any]
-
-type TreeIntStr[K int, V comparable] = Tree[K, V] // made-up dummy example, GOEXPERIMENT=aliastypeparams
-
-func panicIter() {
-	defer func() {
-		if p := recover(); p != nil {
-			println("main panic:", p)
-			panic(p)
-		}
-	}()
-
-	next, _ := iter.Pull(func(yield func(V any) bool) {
-		yield("hello")
-		panic("world")
-	})
-
-	for {
-		fmt.Println(next())
-	}
-}
-
-func sortfn() {
-	const cnt = 20
-	m := make(map[int]struct{}, cnt)
-	for i := range cnt {
-		m[i] = struct{}{}
-	}
-
-	keys := maps.Keys(m)
-	sortedKeys := slices.Sorted(keys)
-	unsortedKeys := slices.Collect(keys)
-	fmt.Printf("sorted: %v; unsorted: %v\n", sortedKeys, unsortedKeys)
-}
-
-func mhash() {
-	s := maphash.MakeSeed()
-	v1, v2 := unique.Make(100), unique.Make(101)
-	h1 := maphash.Comparable(s, v1)
-	h2 := maphash.Comparable(s, v2)
-	fmt.Printf("h1: %v\n", h1)
-	fmt.Printf("h2: %v\n", h2)
-	fmt.Printf("(h1 == h2): %v\n", (h1 == h2))
-	v3, v4 := 100, 100
-	h1, h2 = maphash.Comparable(s, v3), maphash.Comparable(s, v4)
-	fmt.Printf("h1: %v\n", h1)
-	fmt.Printf("h2: %v\n", h2)
-	fmt.Printf("(h1 == h2): %v\n", (h1 == h2))
-}
-
-func wp() {
-	s := new(string)
-	println("original:", s)
-
-	wp := weak.Make(s)
-	runtime.GC() // wp underlaying ptr still needed bc of the next println usage
-
-	ptr := wp.Value()
-	println("value before gc:", ptr, s)
-	runtime.GC()
-
-	ptr = wp.Value()
-	println("value after gc:", ptr)
-}
-
-type TZero struct {
-	A myInt `json:"a,omitzero"`
-}
-
-type myInt int
-
-func (t myInt) IsZero() bool {
-	return t < 1
-}
-
-func ejson() {
-	t := &TZero{A: -1}
-	bb, err := json.Marshal(t)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("string(bb): %v\n", string(bb))
-}
-
-func ejsonv2() {
-	boolMarshaler := jsonv2.MarshalFunc(
-		func(val bool) ([]byte, error) {
-			if val {
-				return []byte(`"✅"`), nil
-			}
-			return []byte(`"🔪"`), nil
-		},
-	)
-
-	val := false
-	bb, err := jsonv2.Marshal(val, jsonv2.WithMarshalers(boolMarshaler))
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("string(bb): %v\n", string(bb))
-}
-
 func main() {
 }
+
+// 1.26
 
 func newexpr() {
 	pi, ps, pf := new(42), new([]string{"42"}), new(func() { _ = 42 })
@@ -221,6 +110,131 @@ func leakProfile() {
 	})
 }
 
+// 1.25
+
+func callJsonv2() {
+	if !strings.Contains(os.Getenv("GOEXPERIMENT"), "jsonv2") {
+		panic("set GOEXPERIMENT=jsonv2")
+	}
+
+	boolMarshaler := jsonv2.MarshalFunc(
+		func(val bool) ([]byte, error) {
+			if val {
+				return []byte(`"✅"`), nil
+			}
+			return []byte(`"🔪"`), nil
+		},
+	)
+
+	val := false
+	bb, err := jsonv2.Marshal(val, jsonv2.WithMarshalers(boolMarshaler))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("string(bb): %v\n", string(bb))
+}
+
+// 1.24
+
+func aliasTypeParams() {
+	// type foo struct {
+	// 	_ structs.HostLayout
+	// }
+
+	type Tree[K cmp.Ordered, V any] struct {
+		left, right *Tree[K, V]
+		key         K
+		value       V
+	}
+
+	type TreeInt = Tree[int, any]
+
+	type TreeIntStr[K int, V comparable] = Tree[K, V] // made-up dummy example, GOEXPERIMENT=aliastypeparams
+}
+
+func maphashCompare() {
+	s := maphash.MakeSeed()
+	v1, v2 := unique.Make(100), unique.Make(101)
+	h1 := maphash.Comparable(s, v1)
+	h2 := maphash.Comparable(s, v2)
+	fmt.Printf("h1: %v\n", h1)
+	fmt.Printf("h2: %v\n", h2)
+	fmt.Printf("(h1 == h2): %v\n", (h1 == h2))
+	v3, v4 := 100, 100
+	h1, h2 = maphash.Comparable(s, v3), maphash.Comparable(s, v4)
+	fmt.Printf("h1: %v\n", h1)
+	fmt.Printf("h2: %v\n", h2)
+	fmt.Printf("(h1 == h2): %v\n", (h1 == h2))
+}
+
+type TZero struct {
+	A myInt `json:"a,omitzero"`
+}
+
+type myInt int
+
+func (t myInt) IsZero() bool {
+	return t < 1
+}
+
+func jsonOmitZero() {
+	t := &TZero{A: -1}
+	bb, err := json.Marshal(t)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("string(bb): %v\n", string(bb))
+}
+
+func weakpointer() {
+	s := new(string)
+	println("original:", s)
+
+	wp := weak.Make(s)
+	runtime.GC() // wp underlaying ptr still needed bc of the next println usage
+
+	ptr := wp.Value()
+	println("value before gc:", ptr, s)
+	runtime.GC()
+
+	ptr = wp.Value()
+	println("value after gc:", ptr)
+}
+
+// 1.23 and older (1.22-)
+
+func seqsSort() {
+	const cnt = 20
+	m := make(map[int]struct{}, cnt)
+	for i := range cnt {
+		m[i] = struct{}{}
+	}
+
+	keys := maps.Keys(m)
+	sortedKeys := slices.Sorted(keys)
+	unsortedKeys := slices.Collect(keys)
+	fmt.Printf("sorted: %v; unsorted: %v\n", sortedKeys, unsortedKeys)
+}
+
+func panicIter() {
+	defer func() {
+		if p := recover(); p != nil {
+			println("main panic:", p)
+			panic(p)
+		}
+	}()
+
+	next, _ := iter.Pull(func(yield func(V any) bool) {
+		yield("hello")
+		panic("world")
+	})
+
+	for {
+		fmt.Println(next())
+	}
+}
+
 func FooIter[E any](s []E) iter.Seq2[int, E] {
 	defer func() {
 		println("immediately after the evaluation of the for loop")
@@ -241,6 +255,8 @@ func FooIter[E any](s []E) iter.Seq2[int, E] {
 		}
 	}
 }
+
+// 1.18
 
 func M2S[S ~[]V, M ~map[K]V, K comparable, V cmp.Ordered](m M) S {
 	s := make(S, 0)
